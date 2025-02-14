53cc702864ec2454c9d71677a848cfbba1616463
vasu Mankotia <vasu.arintech@gmail.com>
2025-02-14 10:45:36 +0530

    End Of Laumching VMs

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ data "terraform_remote_state""network_details" {
   region = "ap-south-1"
  }
 }
+
+resource "aws_instance""my_vm" {
+ ami = "ami-0f2e255ec956ade7f"
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+instance_type = "t2.micro"
+tags = {
+    Name = "vasu-vm1"
+ }
+}
